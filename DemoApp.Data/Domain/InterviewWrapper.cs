51d714dc67ea0bbc78014c7e716d19f3ef10c152
tbrandwood <tim@tblsystems.com>
2021-10-31 18:41:59 +0000

    Populating mock data for Sp5201 - WIP
    Populating mock data for Sp5201 - WIP

---
+++ DemoApp.Data/Domain/InterviewWrapper.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using TSAPI.Public.Domain.Interviews;
+
+namespace DemoApp.Data.Domain
+{
+    public class InterviewWrapper
+    {
+        public DateTime DateLastChanged { get; set; }
+        public bool Complete { get; set; }
+        public Interview Interview { get; set; }
+    }
+}
