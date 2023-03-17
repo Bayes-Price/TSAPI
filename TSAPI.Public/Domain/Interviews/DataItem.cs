538cc2231703365103f8e65429c22f42dd69f39f
tbrandwood <tim@tblsystems.com>
2023-03-17 00:02:37 +0000

    Added Responses child item to DataItem - WIP

--- TSAPI.Public/Domain/Interviews/DataItem.cs
+++ TSAPI.Public/Domain/Interviews/DataItem.cs
@@ -2,9 +2,17 @@
 
 namespace TSAPI.Public.Domain.Interviews
 {
+    /// <summary>Represents the responses given to a survey question</summary>
     public class DataItem
     {
+        /// <summary>The Ident of the question asked</summary>
         public string Ident { get; set; }
+
+        /// <summary>Set of responses given to this question</summary>
+        public List<DataItemValue> Responses { get; set; } = new List<DataItemValue>();
+
+
+        //To Be Discontinued:
         public List<string> Values { get; set; } = new List<string>();
         public List<LoopedDataItem> LoopedDataItems { get; set; }
     }
