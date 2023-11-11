1805364bf4e9510e811648ff28ecd4e65fef527d
tbrandwood <tim@tblsystems.com>
2023-11-11 20:31:14 +0000

    Added summary comments to SurveyDetail class

--- TSAPI.Public/Domain/Metadata/SurveyDetails.cs
+++ TSAPI.Public/Domain/Metadata/SurveyDetails.cs
@@ -2,8 +2,11 @@
 {
     public class SurveyDetail
     {
+        ///<summary>The unique Id that the API system uses internally to identify a given survey</summary>
         public string Id { get; set; }
+        ///<summary>The human readable identifer assigned to the survey within the API system. For example, this may contain an internal job number such as "TS-002"</summary>
         public string Name { get; set; }
+        /// <summary>The human-readable name assigned to the survey within the API system. For example, "Restaurant Satisfaction Survey"</summary>
         public string Title { get; set; }
     }
 }
